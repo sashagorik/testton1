e7a0368db09438d1d562efa10841391bc85da713
sashagorik <139359414+sashagorik@users.noreply.github.com>
2024-06-22 11:41:53 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
-
 // write your NFT miner here
+import {Address, TonClient} from "ton"
+import {getHttpEndpoint} from "@orbs-network/ton-access";
+
+
+async function main () {
+
+  const wallet = Address.parse('YOUR_WALLET_ADDRESS');
+  const collection = Address.parse('COLLECTION_ADDRESS');
+
+}
+
+const endpoint = await getHttpEndpoint({
+    network: "testnet",
+  });
+
+  // initialize ton library
+  const client = new TonClient({ endpoint });
+
+main()
+

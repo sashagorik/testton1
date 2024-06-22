61cc3740a22ea3ae8687485d9edceaec441c50e7
sashagorik <139359414+sashagorik@users.noreply.github.com>
2024-06-22 11:54:29 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@ import {getHttpEndpoint} from "@orbs-network/ton-access";
 
 async function main () {
 
-  const wallet = Address.parse('YOUR_WALLET_ADDRESS');
-  const collection = Address.parse('COLLECTION_ADDRESS');
+  const wallet = Address.parse('0QAK-eku1yCNkL5wt7g9OlBpHSnjadN10h_A19uM3SGVJDA8');
+  const collection = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX');
 
 }
 
